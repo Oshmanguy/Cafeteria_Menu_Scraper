dc9fd51e5791c951f8c32f7026bf6c5986249978
Josh_A <joshagrey1@gmail.com>
2026-01-13 16:53:05 -0600

    added file header

--- web_scraper.py
+++ web_scraper.py
@@ -1,1 +1,7 @@
-print("Test!")
+#FILE HEADER -------------------------------------HEADER
+
+#This python program scraps data from the usask culinary website
+#in order to get data about todays menu. It will then send todays 
+#menu via discord. 
+
+#FILE HEADER -------------------------------------HEADER
