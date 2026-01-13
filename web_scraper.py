74bcc3b936120562f5d737cfe7760705be5a0595
Josh_A <joshagrey1@gmail.com>
2026-01-13 17:07:54 -0600

    import url and request contents

--- web_scraper.py
+++ web_scraper.py
@@ -5,3 +5,22 @@
 #menu via discord. 
 
 #FILE HEADER -------------------------------------HEADER
+
+#imports
+from bs4 import BeautifulSoup
+import requests
+
+#save url to variable 
+web_url = "https://culinaryservices.usask.ca/marquis-culinary-centre/week-2-january12-18-2026.php"
+
+#get contents of web_url at reuqest time and save to varaible 
+web_page = requests.get(web_url)
+
+beautiful_page_contents = BeautifulSoup(web_page.text, "html")
+
+
+
+
+print(beautiful_page_contents.prettify())
+
+
